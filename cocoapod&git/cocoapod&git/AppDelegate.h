6f8797eb1e27bfa25429e140b5ebd3b8ca332776
arduomeng <arduomeng@admindeMac-mini-2.local>
2016-12-09 16:58:59 +0800

    x

---
+++ cocoapod&git/cocoapod&git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cocoapod&git
+//
+//  Created by arduomeng on 16/12/9.
+//  Copyright © 2016年 arduomeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
